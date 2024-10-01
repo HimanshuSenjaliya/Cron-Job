74b2156e9bef57c420ee451ade05d303db7e654f
HimanshuSenjaliya <himanshusenjaliya11@gmail.com>
2024-10-01 15:31:38 +0530

    vjkfbvdkbvd

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require("express");
-const cronJob = require("../cron-job-demo/src/api/cron-job");
 
 const app = express();
-
-app.use("/cron", cronJob);
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
--- src/api/cron-job.js
+++ src/api/cron-job.js
@@ -1,5 +1,5 @@
-export default async function handler(req, res) {
+module.export = async function handler(req, res) {
   console.log("Cron job is running");
 
   res.status(200).json({ message: "Cron job is running....." });
-}
+};
