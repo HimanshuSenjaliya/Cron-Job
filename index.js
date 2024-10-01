0e390199e9cb0775d82d4cd327524ff4977b1a9a
HimanshuSenjaliya <himanshusenjaliya11@gmail.com>
2024-10-01 15:24:42 +0530

    vfnvjbvfjv

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cronJob = require("../cron-job-demo/src/api/cron-job");
 
 const app = express();
+
+app.use("/cron", cronJob);
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
